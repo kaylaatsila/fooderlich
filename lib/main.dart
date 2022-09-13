a9a6d2036176d439214b47eb3a03b11913a8cdf3
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-09-13 08:58:48 +0700

    8th step

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+
+    return MaterialApp(
+      title: 'Fooderlich',
+      theme: theme,
+      home: const Home(),
+    );
+  }
+}
