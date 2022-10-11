5502788af9f0b6d1bec39dd0e1b8f5e0faeaf614
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-10-11 08:21:46 +0700

    1st to 3rd step - add asset, add model

---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/painting.dart';
+
+enum Importance { low, medium, high }
+
+class GroceryItem {
+  String? id;
+  String? name;
+  Importance? importance;
+  Color? color;
+  int? quantity;
+  DateTime? date;
+  bool? isComplete;
+
+  GroceryItem(
+      {this.id,
+      this.name,
+      this.importance,
+      this.color,
+      this.quantity,
+      this.date,
+      this.isComplete = false});
+
+  GroceryItem copyWith(
+      {String? id,
+      String? name,
+      Importance? importance,
+      Color? color,
+      int? quantity,
+      DateTime? date,
+      bool? isComplete}) {
+        
+    return GroceryItem(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        importance: importance ?? this.importance,
+        color: color ?? this.color,
+        quantity: quantity ?? this.quantity,
+        date: date ?? this.date,
+        isComplete: isComplete ?? this.isComplete);
+  }
+}
