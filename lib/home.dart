4fca741c3588619888b1ad21e05e4c185f55877e
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-10-11 08:42:15 +0700

    4th to 7th step - add screen, edit home

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,7 @@ import 'components/components.dart';
 import 'models/models.dart';
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
+import 'screens/grocery_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,22 +18,7 @@ class _HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
     RecipesScreen(),
-    Card3(
-        recipe: ExploreRecipe(
-            title: 'Vegan Trends',
-            tags: [
-              'Healthy',
-              'Vegan',
-              'Carrots',
-              'Greens',
-              'Wheat',
-              'Pescetarian',
-              'Mint',
-              'Lemongrass',
-              'Salad',
-              'Water'
-            ],
-            backgroundImage: 'assets/magazine_pics/mag3.png')),
+    const GroceryScreen()
   ];
 
   void _onItemTapped(int index) {
