ef493f3cc74ebf89ad31c8493557c4c605c6471c
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-10-13 14:13:44 +0700

    fixing stuffs

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fooderlich/screens/recipes_screen.dart';
 import 'components/components.dart';
 import 'models/models.dart';
 import 'screens/explore_screen.dart';
-import 'screens/recipes_screen.dart';
 import 'screens/grocery_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-  
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
+  bool isFavorited = false;
 
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
@@ -29,34 +31,35 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Fooderlich',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-      ),
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        // ignore: prefer_const_literals_to_create_immutables
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: 'Explore',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Recipes',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'To Buy',
-          ),
-        ],
-      ),
+    return Consumer<TabManager>(
+      builder: (context, tabManager, child) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Fooderlich',
+                    style: Theme.of(context).textTheme.headline6)),
+            body: IndexedStack(index: tabManager.selectedTab, children: pages),
+            bottomNavigationBar: BottomNavigationBar(
+                selectedItemColor:
+                    Theme.of(context).textSelectionTheme.selectionColor,
+                currentIndex: tabManager.selectedTab,
+                onTap: (index) {
+                  tabManager.goToTab(index);
+                },
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.explore),
+                    label: 'Explore',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.book),
+                    label: 'Recipes',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.list),
+                    label: 'To Buy',
+                  ),
+                ]));
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'fooderlich_theme.dart';
 import 'home.dart';
+import 'models/models.dart';
 import 'myInheritedWidget.dart';
 
 void main() {
-  runApp(MyInheritedWidget(appData: MyInheritedData(isFavorite: false), child: const Fooderlich()));
+  runApp(MyInheritedWidget(
+      appData: MyInheritedData(isFavorite: false), child: const Fooderlich()));
 }
 
 class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.dark();
-    return MaterialApp(title: 'Fooderlich', theme: theme, home: const Home());
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      title: 'Fooderlich',
+      theme: theme,
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          ChangeNotifierProvider(create: (context) => GroceryManager(),
+          ),
+        ],
+        child: const Home(),
+      ),
+    );
   }
 }
